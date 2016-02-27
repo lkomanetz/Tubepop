4ae6e78b3008d4ce840f17c05d6294fd74751f3e
Logan Komanetz <logan.komanetz@gmail.com>
2016-02-27 12:50:33 -0600

    Creates start of Firefox plugin.

---
+++ Firefox/README.md
@@ -0,0 +1,2 @@
+#Tubepop
+Allows you to undock a YouTube video from the site into its own window.
---
+++ Firefox/firefoxExtension.js
@@ -0,0 +1,54 @@
+var buttons = require("sdk/ui/button/action");
+var notifications = require("sdk/notifications");
+var tabs = require("sdk/tabs");
+var windows = require("sdk/windows").browserWindows;
+var regex = /(\w+:\/\/\w{3}\.\w+\.(com|org|gov|be))(\/\w+\?\w{1}=(.+?)(?!\w+))/;
+
+var button = buttons.ActionButton({
+    id: "tubepop",
+    label: "Tubepop",
+    icon: {
+        "16": "./button_black_eject_16.png",
+        "32": "./button_black_eject_32.png",
+        "64": "./button_black_eject_64.png"
+    },
+    onClick: handleClick
+});
+
+function handleClick(state) {
+    if (!isCurrentTabYoutube()) {
+        notifications.notify({
+            text: "I'm sorry:(\nYour current tab is not youtube.",
+            iconURL: "./button_black_eject_64.png"
+        });
+        
+        return;
+    }
+    else {
+        var embedLink = getEmbedLink();
+        var tab = tabs.activeTab;
+        
+        windows.open({
+            url: embedLink,
+            onOpen: function(window) {
+                tab.close();
+            }
+        });
+    }
+}
+
+function getEmbedLink() {
+    var match = regex.exec(tabs.activeTab.url);
+    return match[1] + "/embed/" + match[4];
+}
+
+function isCurrentTabYoutube() {
+    var match = regex.exec(tabs.activeTab.url);
+    var isYoutube = false;
+    
+    if (match) {
+        isYoutube = match[0].indexOf("youtube") != -1;
+    }
+    
+    return isYoutube;
+}
