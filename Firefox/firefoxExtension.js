94b8c4576fdbed81d27ab6bba2d3977df52b5836
Logan Komanetz <logan.komanetz@gmail.com>
2016-03-05 14:45:32 -0600

    Trying to get firefox plugin to get current play time.

--- Firefox/firefoxExtension.js
+++ Firefox/firefoxExtension.js
@@ -2,6 +2,8 @@ var buttons = require("sdk/ui/button/action");
 var notifications = require("sdk/notifications");
 var tabs = require("sdk/tabs");
 var windows = require("sdk/windows").browserWindows;
+var data = require("sdk/self").data;
+var pageMod = require("sdk/page-mod");
 var panels = require("sdk/panel");
 var frame = require("sdk/ui/frame");
 var regex = /(\w+:\/\/\w{3}\.\w+\.(\w{3}|\w{2}))(\/\w+\?\w{1}=([^\&]*))/;
@@ -15,6 +17,11 @@ var button = buttons.ActionButton({
         "64": "./button_black_eject_64.png"
     },
     onClick: handleClick
+});
+
+pageMod.PageMod({
+    include: "*.youtube.com",
+    contentScriptFile: data.url("scripts/clientScript.js")
 });
 
 function handleClick(state) {
